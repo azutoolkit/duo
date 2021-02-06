a101f29671098f7bc0eee77fdfabcc80e0c0ead9
Elias J. Perez <eliasjpr@gmail.com>
2021-02-05 20:14:33 -0500

    Move hpack

--- src/duo/hpack.cr
+++ src/duo/hpack.cr
@@ -6,7 +6,7 @@ module Duo
 
     def self.encoder
       HPACK::Encoder.new(
-        max_table_size: local_settings.header_table_size,
+        max_table_size: DEFAULT_HEADER_TABLE_SIZE,
         indexing: HPACK::Indexing::NONE,
         huffman: true
       )
@@ -14,7 +14,7 @@ module Duo
 
     def self.decoder
       HPACK::Decoder.new(
-        max_table_size: remote_settings.header_table_size
+        max_table_size: DEFAULT_HEADER_TABLE_SIZE
       )
     end
   end
